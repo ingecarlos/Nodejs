2587cc201e3ecce7b07c929cf4b1eb6f5a50304c
Carlos Aguilar <carlos.ricardo.crac@gmail.com>
2021-12-08 18:44:10 -0600

    setup socket io

--- projects/chat_api/index.js
+++ projects/chat_api/index.js
@@ -1,15 +1,24 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const { Console } = require('console');
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.use(cors({
-    origin: [
-        'http://localhost:3000' //comes from REACT
-    ]
-}))
+io.on('connection',(socket)=>{
+    console.log('we have a new connection');
 
-app.use(express.json())
+    socket.on('disconnect', ()=>{
+        console.log('user had left');
+    });
+});
 
-app.listen(8000);
-console.log('listening on port 8000');
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server running: listening on port ${PORT}`) );
